16163bb4fef7e332eb74336537ad7b6c6f675767
Keiaa <confinedrose@duck.com>
2026-04-24 17:09:42 +0800

    refactor: drop module-templates

--- packages/module-template/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { mount } from 'svelte'
-import App from './App.svelte'
-
-// Initialize Svelte 5 app on the embed container
-const app = mount(App, { target: document.getElementById('app')! })
-
-export default app
